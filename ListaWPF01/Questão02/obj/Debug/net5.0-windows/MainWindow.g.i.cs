b8f7a576517e8ab2f742ca632206b3313e2470e8
cossito <h.cossito@escolar.ifrn.edu.br>
2022-11-24 22:14:54 -0300

    Upload da questão 03

--- ListaWPF01/Questão02/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ ListaWPF01/Questão02/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace Questão02 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Questão02;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Questão02;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ ListaWPF01/Questão03/retangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão03
+{
+    internal class retangulo
+    {
+        private double b;
+        private double h;
+        public void SetBase(double b)
+        {
+            if (b > 0) this.b = b;
+        }
+        public void SetAltura(double h)
+        {
+            if (h > 0) this.h = h;
+        }
+        public double GetBase()
+        {
+            return b;
+        }
+        public double GetAltura()
+        {
+            return h;
+        }
+        public double CalcArea()
+        {
+            return b * h;
+        }
+        public double CalcDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2));
+        }
+    }
+}
