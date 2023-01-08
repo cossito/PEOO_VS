fce76e74e50b350ff6943bcdddef7dcb7f773cd6
cossito <h.cossito@escolar.ifrn.edu.br>
2023-01-08 19:07:52 -0300

    Atualizações: Classe Aluno

---
+++ EscolaApp/EscolaApp/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Email { get; set; }
+        public int IdTurma { get; set; }
+    }
+}
---
+++ EscolaApp/EscolaApp/NAluno.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EscolaApp
+{
+    static class NAluno
+    {
+        private static List<Aluno> alunos = new List<Aluno>();
+        public static void Inserir(Aluno a)
+        {
+            Abrir();
+            alunos.Add(a);
+            Salvar();
+        }
+        public static List<Aluno> Listar()
+        {
+            Abrir();
+            return alunos;
+        }
+        public static void Excluir(Aluno a)
+        {
+            Abrir();
+            Aluno x = null;
+            // percorrer a lista de alunos procurando ao id informado (a.Id)
+            foreach (Aluno obj in alunos)
+                if (obj.Id == a.Id) x = obj;
+            if (x != null) alunos.Remove(x);
+            Salvar();
+        }
+        public static void Atualizar(Aluno a)
+        {
+            Abrir();
+            // percorrer a lista de aluno procurando ao id informado (a.Id)
+            foreach (Aluno obj in alunos)
+                if (obj.Id == a.Id)
+                {
+                    obj.Nome = a.Nome;
+                    obj.Matricula = a.Matricula;
+                    obj.Email = a.Email;
+                }
+            Salvar();
+        }
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try
+            {
+                // objeto que transforma uma lista de turmas em texto em XML
+                XmlSerializer xml = new XmlSerializer(typeof(List<Aluno>));
+                // objeto que abre um texto em um arquivo
+                f = new StreamReader("./turmas.xml");
+                // chama a operação de desserialização informando o destino do texto XML
+                alunos = (List<Aluno>)xml.Deserialize(f);
+                // fecha o arquivo
+            }
+            catch
+            {
+                alunos = new List<Aluno>();
+            }
+            if (f != null) f.Close();
+        }
+        public static void Salvar()
+        {
+            // objeto que transforma uma lista de turmas em texto em XML
+            XmlSerializer xml = new XmlSerializer(typeof(List<Aluno>));
+            // objeto que grava um texto em um arquivo
+            StreamWriter f = new StreamWriter("./turmas.xml", false);
+            // chama a operação de serialização informando o destino do texto XML
+            xml.Serialize(f, alunos);
+            // fecha o arquivo
+            f.Close();
+        }
+        public static void Matricular(Aluno a, Turma t)
+        {
+
+        }
+        public static List<Aluno> Listar(Turma t)
+        {
+
+        }
+    }
+}
